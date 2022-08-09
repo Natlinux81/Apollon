14d6e027f9c43202abdaf05d1679b3fc844926f5
Natlinux81 <97396587+Natlinux81@users.noreply.github.com>
2022-08-09 15:05:35 +0200

    Initial commit

---
+++ Apollon.WPF/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Apollon.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void Button_Exit(object sender, RoutedEventArgs e)
+        {
+           this.Close();
+        }
+
+        private void Button_Max(object sender, RoutedEventArgs e)
+        {
+            
+            if (this.WindowState == WindowState.Maximized)
+                
+            {
+                this.WindowState = WindowState.Normal;    
+            }
+            else
+
+            this.WindowState = WindowState.Maximized;
+
+        }
+
+        private void Button_Min(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+    }
+}
---
+++ Apollon.WPF/ViewModels/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apollon.WPF.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPrpertyChanged(string properyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(properyName));
+        }
+    }
+}
