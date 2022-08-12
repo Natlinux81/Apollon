bb3394b03f6e9406cab82bb2608ace7e07ad2af9
Natlinux81 <97396587+Natlinux81@users.noreply.github.com>
2022-08-12 15:46:04 +0200

    StartView finished

--- Apollon.WPF/ViewModels/ApollonOverviewDetailsViewModel.cs
+++ Apollon.WPF/ViewModels/ApollonOverviewDetailsViewModel.cs
@@ -12,15 +12,15 @@ namespace Apollon.WPF.ViewModels
     {
         private readonly SelectedTournamentStore _selectedTournamentStore;
 
-        private Tournament _selectedTournament => _selectedTournamentStore.SelectedTournament; 
+        private Tournament SelectedTournament => _selectedTournamentStore.SelectedTournament; 
 
-        public bool hasSelectedTournament => _selectedTournament != null;
-        public string Organisation => _selectedTournament?.Organisation ?? "keine Organisation";
-        public string Tournamentname => _selectedTournament?.Tournamentname ?? "kein Name";
-        public string Category => _selectedTournament?.Category ?? "keine Kategorie";
-        public string Startdate => _selectedTournament?.Startdate ?? "kein Datum";
-        public string Enddate => _selectedTournament?.Enddate ?? "kein Datum";
-        public string Location => _selectedTournament?.Location ?? "kein Ort";
+        public bool HasSelectedTournament => SelectedTournament != null;
+        public string Organisation => SelectedTournament?.Organisation ?? "keine Organisation";
+        public string Tournamentname => SelectedTournament?.Tournamentname ?? "kein Name";
+        public string Category => SelectedTournament?.Category ?? "keine Kategorie";
+        public string Startdate => SelectedTournament?.Startdate ?? "kein Datum";
+        public string Enddate => SelectedTournament?.Enddate ?? "kein Datum";
+        public string Location => SelectedTournament?.Location ?? "kein Ort";
 
         public ApollonOverviewDetailsViewModel(SelectedTournamentStore selectedTournamentStore)
         {
@@ -37,7 +37,7 @@ namespace Apollon.WPF.ViewModels
 
         private void SelectedTournamentStore_SelectedTournamentChanged()
         {
-            OnPropertyChanged(nameof(hasSelectedTournament));
+            OnPropertyChanged(nameof(HasSelectedTournament));
             OnPropertyChanged(nameof(Organisation));
             OnPropertyChanged(nameof(Tournamentname));
             OnPropertyChanged(nameof(Category));
